9a9460a4858999740842a7193408554fa7e61173
7eet <raisimranjeetsingh@gmail.com>
2019-09-07 13:25:17 +0530

    added method using stack

--- ReverseString.java
+++ ReverseString.java
@@ -1,16 +1,31 @@
-
+import java.util.Stack;
 public class ReverseString{
     public static void main(String[] args){
 	String string = "Hello this is a new String.";
-	reverseString(string);	
+	System.out.println("Given string: "+string);
+	System.out.println("Reversed String is: "+reverseUsingArray(string));	
     }
-
-    private static void reverseString(String string){
+	
+	// using char array
+    private static String reverseUsingArray(String string){
 	char[] charArray = string.toCharArray();
 	StringBuilder sb = new StringBuilder();
 	for(var i=charArray.length-1;i>=0;i--)
 	    sb.append(charArray[i]);
 	    
-	System.out.println(sb);
-    } 
+	return sb.toString();
+    }
+    
+    // using character stack
+   private static String reverseUsingStack(String input){
+		Stack<Character> charStack = new Stack<>();
+		for(var i=0;i<input.length();i++){
+			charStack.push(input.charAt(i));
+		}
+		StringBuilder reversedString = new StringBuilder();
+		while(!charStack.empty()){
+			reversedString.append(charStack.pop());
+		}
+		return reversedString.toString();
+	} 
 }
